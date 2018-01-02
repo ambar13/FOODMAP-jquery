a1576a4599097a3b1526a4a24c32fd33d3233a7e
ambar13 <ambar13_9@hotmail.com>
2018-01-02 02:09:19 -0500

    realizndo pruebas

--- js/page.js
+++ js/page.js
@@ -1,20 +1,21 @@
 $(document).ready(function() {
+  var $imgPlate = $('.plate');
   var $plate = $('.plate');  
   var $listFood = $('#listFood'); 
-  var $closeModal = $('button.close');
 
 
 // Seleccion de el combo-box
-// $listFood.click(function() {
-//     var value = $(this).val();    
-//     $plate.hide();
-//     $('.plate[=''+ value +'']').show();
-//   });
+$listFood.click(function() {
+    var selectP = $(this).val();    
+  $imgPlate.hide();
+  $('.plate[data-plate="' + selectP + '"]').show();
+});
 
+ 
 // efecto del mouse
 $('.plate').mouseover(function() {
-  $('.plate').css('height','200px');  
-  $('.plate').css('width','300px');
+  $('.plate').css('height' , '200px');  
+  $('.plate').css('width' , '300px');
 });
 $('.plate').mouseout(function() {
 $('.plate').css('height','120px');
